ea80e4d1e2c4c0855afb795d76c6669108135e7d
Mohammed Omar <omarmd2311@gmail.com>
2018-11-07 10:40:54 +0530

    palindrome

---
+++ all programs/src/Palindrome.java
@@ -0,0 +1,18 @@
+
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		String original;
+		System.out.println("Enter Value:");
+		Scanner sc=new Scanner(System.in);
+		original=sc.next();
+		if(original.equals(new StringBuffer(original).reverse().toString()))   //we have used stringbuffer so that we can use built in reverse() of StringBuffer class using this we can check palindrome of numbers and strings
+				System.out.println("palindrome");
+		else
+			System.out.println("not a palindrome");
+	}
+}
+
